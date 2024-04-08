608504a38fc7d90a3b55e3acf128fd1d4ad9c771
waleedkhalid777 <wk4721260@gmail.com>
2024-04-08 17:33:58 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,44 @@
+import inquirer from   "inquirer";
+
+let todos= [];
+while (true){
+    let input = await inquirer.prompt ([
+
+{
+
+
+name: "Todoitem",
+type: "input",
+message: 'add todo item in listing ',
+    
+},
+{
+
+ name: "addmore",
+ type: "list",
+ choices: ["yes", "no"]
+
+}    
+]);
+
+// output of program
+const{Todoitem, addmore}= input;
+
+todos.push(Todoitem);
+if (addmore == "no"){
+    console.log("Todolist");
+
+    // add more item
+    for (let i=0;i<todos .length; i++)
+    {console.log(todos[i])}
+
+// exit loop
+break;
+
+
+
+
+}
+
+
+}
